15a67452e6acfefb8198bad14d9f45a84fd3baad
Thais Perdomo <89431979+ThaisPerdomo@users.noreply.github.com>
2023-01-31 15:14:51 -0300

    imagens adicionadas na função JS, no .json e tags inseridas no html

--- my1stwebsite/js/scripthome.js
+++ my1stwebsite/js/scripthome.js
@@ -1,12 +1,14 @@
 /**
  * A função abaixo insere as noticias do arquivo json na pasta. 
- * Precisa ter, no HTML, elementos com as classes passadas nos parâmetros da função.
+ * Precisa ter, no HTML, os seguintes elementos: divs para título, divs para texto e img para imagem com o src vazio.
+ * Os parâmetros da função são os nomes das classes dos elementos do HTML.
  * Importante ressaltar que deve ter, no mínimo, o mesmo número de elementos que o número de notícias no arquivo .json. 
  * Se tiver menos, vai exibir no console log que tem menos e não vai colocar as notícias na página
  */
-function insereNoticiaDoJson(classTitulo, classTexto){
+function insereNoticiaDoJson(classTitulo, classTexto, classImagem){
     let htmlTitulo = document.querySelectorAll(classTitulo);
     let htmlTexto = document.querySelectorAll(classTexto);
+    let htmlImagem = document.querySelectorAll(classImagem);
 
     fetch('jsons/noticias.json').then(res => res.json()).then(noticia => {
 
@@ -18,11 +20,14 @@ function insereNoticiaDoJson(classTitulo, classTexto){
                 for(i = 0; i < noticia.length; i++){
                     htmlTitulo[i].innerHTML = noticia[i].titulo;
                     htmlTexto[i].innerHTML = noticia[i].texto;
+                    htmlImagem[i].setAttribute("src", noticia[i].imagem);
                 }
             }
         })
 }
 
-insereNoticiaDoJson(".tituloNoticia", ".textoNoticia");
+insereNoticiaDoJson(".tituloNoticia", ".textoNoticia", ".fotoNoticia");
+
+
 
 
